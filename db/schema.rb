86c80534ca877dcace934a871713494af3192b7d
Alex Ivanov <alex.ivanov.space@gmail.com>
2015-05-12 14:23:34 +0300

    Add model Testimonials

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150512112300) do
+
+  create_table "testimonials", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "feedback"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
