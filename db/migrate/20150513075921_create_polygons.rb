639179a2cdef039abb26e8e0725085bf419b1ebc
Alex Ivanov <alex.ivanov.space@gmail.com>
2015-05-13 10:59:46 +0300

    Add model Polygon

---
+++ db/migrate/20150513075921_create_polygons.rb
@@ -0,0 +1,10 @@
+class CreatePolygons < ActiveRecord::Migration
+  def change
+    create_table :polygons do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513075846) do
+ActiveRecord::Schema.define(version: 20150513075921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20150513075846) do
   end
 
   create_table "owners", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "polygons", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
