0d10035e893f16c535122381bac67fa01a8e7c3f
Alex Ivanov <alex.ivanov.space@gmail.com>
2015-06-08 10:59:40 +0300

    Update Javascript

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,3 +1,12 @@
 class Testimonial < ActiveRecord::Base
   validates :user_name, :feedback, presence: true
+  before_save :censorship
+
+  private
+
+  def censorship
+    if feedback =~ /\bshit\b/
+      self.feedback = feedback.downcase.gsub! /\bshit\b/, 'fun'
+    end
+  end
 end
